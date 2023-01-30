3d57533c621e31678425cf6eaa645882c2389519
Sarvani Subramanian <sarvani@Sarvanis-MacBook-Air.local>
2023-01-30 14:16:48 +0000

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+
+//const packageJson = require("./package.json");
+//import * as packageJson from "./package.json";
+
+export default [
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                file:"dist/cjs/index.js",
+                format: "cjs",
+                sourcemap: true,
+            },
+            {
+                file:"dist/esm/index.js",
+                format: "esm",
+                sourcemap: true,
+            },
+        ],
+        plugins: [
+            resolve(),
+            commonjs(),
+            typescript({ tsconfig: "./tsconfig.json" }),
+        ],
+    },
+    {
+        input: "dist/esm/types/index.d.ts",
+        output: [{ file: "dist/index.d.ts", format: "esm" }],
+        plugins: [dts()],
+    }
+];
