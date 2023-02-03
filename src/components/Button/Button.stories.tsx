06631db810f6d2876e741625455688aa183e6875
Sarvani Subramanian <sarvani@Sarvanis-MacBook-Air.local>
2023-02-03 23:07:33 +0000

    adding children prop in button

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -17,6 +17,7 @@ export const HelloWorld = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 HelloWorld.args = {
   label: 'Hello World',
+  onClick: () => alert("It's working!!")
 };
 
 export const ClickMe = Template.bind({});
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,11 +3,12 @@ import './Button.css';
 
 export interface ButtonProps {
     label: string;
+    children?: React.ReactNode;
     onClick?: () => void;
 }
 
 const Button = (props: ButtonProps) => {
-    return <button onClick={props.onClick}>{props.label}</button>;
+    return <button onClick={props.onClick}>{props.label}{props.children}</button>;
 };
 
 export default Button;
