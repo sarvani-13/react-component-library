23561f1214d1b0eb9a9dd1af6f5e651a0771a518
Sarvani Subramanian <sarvani@Sarvanis-MacBook-Air.local>
2023-02-03 15:06:44 +0000

    tests, storybook and css

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
 
 //const packageJson = require("./package.json");
 //import * as packageJson from "./package.json";
@@ -25,11 +26,13 @@ export default [
             resolve(),
             commonjs(),
             typescript({ tsconfig: "./tsconfig.json" }),
+            postcss()
         ],
     },
     {
         input: "dist/esm/types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
         plugins: [dts()],
+        external: [/\.css$/],
     }
 ];
